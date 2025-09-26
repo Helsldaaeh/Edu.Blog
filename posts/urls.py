5ca26c52ccd9f26171650b48f713779ac4b25364
Helsldaaeh <117380436+Helsldaaeh@users.noreply.github.com>
2025-09-26 21:04:12 +0300

    Some changes

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,16 @@
 ﻿from django.shortcuts import redirect
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from posts import views
 
 urlpatterns = [
     path('post/', views.post, name='post-list'),
+    path('post/<int:post_id>', views.retrieve, name="retrieve"),
+    path('post/<int:post_id>/update', views.update, name="update"),
+    path('post/<int:post_id>/delete/', views.delete, name="delete"),
     path('post/create', views.create, name='post-create'),
     path('login/', views.login, name='login'),
     path('about/', views.about, name='about'),
+    #path('login/', auth_views.LoginView.as_view(template_name='posts/login.hrml'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-
+from django import get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from posts.forms import PostForm
 from posts.models import Post
+from forms import UserForm
 
 
 def post(request: HttpRequest) -> HttpResponse:
@@ -24,9 +27,53 @@ def create(request: HttpRequest) -> HttpResponseRedirect | HttpResponse | None:
     return render(request, 'posts/create_post.html', {'form': form})
 
 
+def retrieve(request: HttpRequest, post_id:int) -> HttpResponse:
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'posts/retrieve.html', {'post':post})
+
+def update(request: HttpRequest, post_id:int) -> HttpResponse:
+    post = get_object_or_404(Post, id=post_id)
+    if (request.method == 'POST'):
+        form = PostForm(request.POST)
+        if (form.is_valid()):
+            post.title = form.cleaned_data('title')
+            post.content = form.cleaned_data('content')
+            post.save()
+            return redirect('posts')
+    else:
+        form = PostForm(initial={
+            'title':post.title,
+            'content':post.content,
+        })
+    return render(request, 'posts/update.html', {'form':form, 'post':post})
+
+def delete(request:HttpRequest, post_id:int) -> HttpResponse:
+    post = get_object_or_404(Post, id=post_id)
+    if (request.method == 'POST'):
+        post.delete()
+        return redirect('posts')
+    return render(request, 'posts/delete.html', {'post':post})
+
+
 def about(request: HttpRequest) -> HttpResponse:
     return render(request, 'posts/about.html', {'title': 'About us'})
 
-
 def login(request: HttpRequest) -> HttpResponse:
-    return render(request, 'posts/login.html', {'title': 'Login'})
+    if request.method == 'POST':
+        form = UserForm(request.Post)
+        if form.is_valid():
+            username = form.cleaned_data('username')
+            password = form.cleaned_data('password')
+            user = authenticate(
+                request,
+                username=username,
+                password=password,
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('posts')
+            else:
+                messages.error(request, 'Не верное имя пользователя, или пароль')
+    else:
+        form = UserForm()
+    return render(request, 'posts/login.html', {'form':form})
